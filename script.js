1c7cc4e5f0e03fb8744e5301636ebc597931c82a
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-04-04 21:19:32 +0100

    displaying list

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds, addData } from "./storage.js";
+import { getUserIds, addData, getData } from "./storage.js";
+
+document.getElementById("input-form").style.display = "none";
+
+
 function dropDown(){
     const users = getUserIds();
     let dropDownList = document.getElementById("dropdown");
@@ -17,29 +21,62 @@ function dropDown(){
         dropDownList.appendChild(optionDropdown);
     }
     dropDownList.addEventListener("change", ()=>{
-    let h1 = document.createElement("h1")
-    h1.innerHTML = `hello`;
-    document.body.appendChild(h1);
+        document.getElementById("ul-list").innerHTML= "";
+        document.getElementById("input-form").style.display = "block";
+
+        let opt = dropDownList.options[dropDownList.selectedIndex];
+        let id = opt.id;
+
+        addDataLocal(id)
+        displayList(id)
     
 })
 }
 
-function addDataLocal(){
+function addDataLocal(id){
     let topic = document.getElementById("topic");
     let datePicker = document.getElementById("datePicker");
-    let data = {topic: topic.value, date: datePicker.value}
-    console.log(data)
-    let dropdown = document.getElementById("dropdown");
-    dropdown.addEventListener("change", function() {
-    let opt = dropdown.options[dropdown.selectedIndex];
-    console.log("iddd" + opt.id)
+    
+    let userId = id;
+    let button = document.getElementById("btn")
+    button.addEventListener("submit", (e)=> {
+        e.preventDefault()
+        document.getElementById("ul-list").innerHTML= "";
+        
+        let data = {topic: topic.value, date: datePicker.value}
+        console.log(data);
+        addData(userId, data);
+        displayList(userId);
+
+        topic.value = ""; // empty the topic input after adding
+        datePicker.value = new Date().toISOString().split('T')[0]; // set as default date again
     })
+    
 } 
+
+function displayList(id){
+    let ulList = document.getElementById("ul-list");
+    let userId = id;
+    const receivedData = getData(userId);
+    if(!receivedData){
+        let li = document.createElement("li");
+        li.innerHTML = "There is no agenda";
+        ulList.appendChild(li);
+    }
+    else{
+        for(let i of receivedData){
+            let li = document.createElement("li");
+            li.innerHTML = `${i.topic} - ${i.date}`;
+            ulList.appendChild(li);
+        }
+    }
+    
+}
 
 window.onload = function () {
     let datePickerDefault = document.getElementById("datePicker");
     datePickerDefault.value = new Date().toISOString().split('T')[0];
     dropDown()
-    addDataLocal()
+    
   //document.querySelector("body").innerText = `There are ${users.length} users`;
 };
